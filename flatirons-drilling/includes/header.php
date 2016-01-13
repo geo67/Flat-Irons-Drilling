ce939ef6526d2de3425843c7b0216c62ea39e83a
George Robinson <george-robinson@georges-macbook-pro.local>
2016-01-12 17:42:08 -0700

    flatirons drilling first commit

---
+++ flatirons-drilling/includes/header.php
@@ -0,0 +1,17 @@
+			<div id="header"> <!-- start header-->
+
+				<a href="index.html">
+					<div class="logo-cont">
+						<img src="imgs/logo.png" alt="Flatirons Drilling Logo"/>
+					</div>
+				</a>
+				<ul class="address">
+
+					<li>2009 2nd Avenue</li>
+					<li>Greeley, Colorado 80631</li>
+					<li>PHONE<span>970.356.1444</span></li>
+					<li>FAX	<span>970.353.0222</span></li>
+
+				</ul>
+
+			</div><!-- end header-->
